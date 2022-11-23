89d98589d0a45d6ce635ccc4b3077211d5fb880a
Kesavan <116025852+Kesavan-E@users.noreply.github.com>
2022-11-23 22:34:27 +0530

    Create ColorPairImpl.java

---
+++ colorcoder/ColorPairImpl.java
@@ -0,0 +1,29 @@
+package colorcoder;
+
+public class ColorPairImpl {
+
+	private ColorPairImpl() {
+		
+	}
+	
+	public static ColorPair getColorFromPairNumber(int pairNumber) {
+		int zeroBasedPairNumber = pairNumber - 1;
+		MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / Main.NUMBEROFMINORCOLORS);
+		MinorColor minorColor = MinorColor.fromIndex(zeroBasedPairNumber % Main.NUMBEROFMINORCOLORS);
+		return new ColorPair(majorColor, minorColor);
+	}
+
+	public static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
+		return major.getIndex() * Main.NUMBEROFMINORCOLORS + minor.getIndex() + 1;
+	}
+	
+	static void printManual() {
+        int count = 1;
+        for(int i = 0 ; i< Main.NUMBEROFMAJORCOLORS ; i++) {
+            for(int j = 0; j< Main.NUMBEROFMINORCOLORS ; j++) {
+                 System.out.println(count + " | " + Main.MajorColorNames[i] + " | " + Main.MinorColorNames[j]);
+                 count++;
+            }
+        }
+   } 
+}
