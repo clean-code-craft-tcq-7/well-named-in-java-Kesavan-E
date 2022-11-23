cb60e9006d85e0261b65c1a757a5ea39b40d2cb3
Kesavan <116025852+Kesavan-E@users.noreply.github.com>
2022-11-23 13:37:21 +0530

    Create ColorPair.java

---
+++ colorcoder/ColorPair.java
@@ -0,0 +1,23 @@
+package colorcoder;
+public class ColorPair {
+        private MajorColor majorColor;
+        private MinorColor minorColor;
+        
+        public ColorPair(MajorColor major, MinorColor minor)
+        {
+            majorColor = major;
+            minorColor = minor;
+        }
+        public MajorColor getMajor() {
+            return majorColor;
+        }
+        public MinorColor getMinor() {
+            return minorColor;
+        }
+        String ToString() {
+            String colorPairStr = MajorColorNames[majorColor.getIndex()];
+            colorPairStr += " ";
+            colorPairStr += MinorColorNames[minorColor.getIndex()];
+            return colorPairStr;
+        }
+    };
